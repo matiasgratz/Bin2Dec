fd80d4c852bda9f0823ba6800ab8539b8436516a
matiasgratz <matiasgratz@frba.utn.edu.ar>
2022-02-02 12:33:26 -0300

    askBinary: left save the number

---
+++ Bin2Dec.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void askBinary(){
+    int binary;
+    printf("Enter binary: \n");
+    scanf("%d", &binary);
+}
+
+int main(){
+    askBinary();
+    checkIfBinary();
+    return 0;
+}
+
+void checkIfBinary(){
+
+
+
+}
